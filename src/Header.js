e5945bfbb749d128074eafedbf8eb4a27d2322dc
Jaroslaw Charchula <charchula@gmail.com>
2022-05-21 18:53:55 +0200

    add right header options with css.

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
-
+import HeaderOption from './HeaderOption.js';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 function Header() {
   return (
@@ -15,8 +20,14 @@ function Header() {
           </div>
         </div>
 
-        <div className="header__right">
 
+        <div className="header__right">
+          <HeaderOption Icon={HomeIcon} title='Home'/>
+          <HeaderOption Icon={SupervisedUserCircleIcon} title='My Network'/>
+          <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
+          <HeaderOption Icon={ChatIcon} title='Messaging'/>
+          <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+          <HeaderOption avatar= "https://cdn.pixabay.com/photo/2017/08/01/08/29/woman-2563491_960_720.jpg" title='me' />
         </div>        
     </div>
   )
