33b759508df87827d7f16b95f599f07ec6ed496f
Jaroslaw Charchula <charchula@gmail.com>
2022-05-21 23:28:28 +0200

    add post and css

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Feed.css"
 import CreateIcon from '@mui/icons-material/Create';
 import InputOption from './InputOption.js'
@@ -6,8 +6,17 @@ import ImageIcon from '@mui/icons-material/Image';
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 import EventNoteIcon from '@mui/icons-material/EventNote';
 import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
+import Post from './Post';
 
 function Feed() {
+    const [post, setPost] = useState([]);
+
+    const sendPost = e => {
+        e.preventDefault();
+
+        setPost();
+    }
+
     return (
         <div className="feed">
             <div className="feed__inputContainer">
@@ -15,7 +24,7 @@ function Feed() {
                     <CreateIcon />
                     <form>
                         <input type="text" />
-                        <button type="submit">Send</button>
+                        <button onClick={sendPost} type="submit">Send</button>
                     </form>
                 </div>
                 <div className="feed__inputOptions">
@@ -29,7 +38,12 @@ function Feed() {
                 </div>
             </div>
             
-            {/* Post */}
+            {post.map((post) => (
+                <Post />
+            ))}
+            <Post name='Johnny' 
+            descryption = 'etst is a test' 
+            message='WOW this work'/>
         </div>
     )
 }
